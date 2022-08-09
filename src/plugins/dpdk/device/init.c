6a75d2df01ef70bd778152e9e374153fe5c762ff
Sachin Saxena <sachin.saxena@oss.nxp.com>
2022-08-09 15:08:04 +0530

    Fix copyright issues
    Signed-off-by: Sachin Saxena <sachin.saxena@oss.nxp.com>

--- src/plugins/dpdk/device/init.c
+++ src/plugins/dpdk/device/init.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 Cisco and/or its affiliates.
+ * Copyright 2019-2022 NXP
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
